08a21f9132d61dde2ec818e673d27454d326a134
Nathscrespo <ctingo@espol.edu.ec>
2023-11-08 16:38:58 -0500

    Creacion de codigo
    Continuacion de codigo de traduccion de diagrama a codigo

---
+++ Taller05UML_Codigo/src/main/java/ec/edu/espol/taller05uml_codigo/Taller05UML_Codigo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.taller05uml_codigo;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller05UML_Codigo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
