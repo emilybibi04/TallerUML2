f959c87410d0142fafa61017727efc442ebc73dd
TingoCarlos08 <107369011+TingoCarlos08@users.noreply.github.com>
2023-11-08 16:56:36 -0500

    Update Usuario.java

--- Taller05UML_Codigo/src/main/java/ec/edu/espol/taller05uml_codigo/Usuario.java
+++ Taller05UML_Codigo/src/main/java/ec/edu/espol/taller05uml_codigo/Usuario.java
@@ -4,10 +4,77 @@
  */
 package ec.edu.espol.taller05uml_codigo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author CltControl
  */
 public class Usuario {
+    protected String usuario;
+    protected String contraseña;
+    protected String nombre;
+    protected String apellido;
+    public ArrayList<Incidente> incidentes = new ArrayList();
+    protected boolean login(){
+        
+        return true;
+    }
+    
+    protected boolean logout(){
+        return true;
+    }
+
+    public Usuario(String usuario, String contraseña, String nombre, String apellido) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public ArrayList<Incidente> getIncidentes() {
+        return incidentes;
+    }
+
+    public void setIncidentes(ArrayList<Incidente> incidentes) {
+        this.incidentes = incidentes;
+    }
+    
+    
+    
+    
+    
     
 }
